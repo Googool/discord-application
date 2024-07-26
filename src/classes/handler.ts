4da407c44451a02c6cf682ff52cfe8f2f40c9e4c
Ask Marius Lysgaard <askx0113@gmail.com>
2024-07-26 13:08:06 +0200

    Small rework on the handlers. Adding a load_files function for simplicity

--- src/classes/handler.ts
+++ src/classes/handler.ts
@@ -10,53 +10,63 @@ export class Handler {
     this.client = client;
   }
 
-  private clearCache(modulePath: string): void {
-    const resolvedPath = require.resolve(modulePath);
-    if (require.cache[resolvedPath]) {
-      delete require.cache[resolvedPath];
+  public async load_events(): Promise<void> {
+    const eventDir = this.load_files(`${process.cwd()}/dist/events`);
+    for (const file of eventDir) {
+      if (file.endsWith('.ts') || file.endsWith('.js')) {
+        const eventPath = path.resolve(file);
+        this.clear_cache(eventPath);
+        const event: Event = (await import(eventPath)).default;
+        const execute = (...args: any[]) => event.execute(...args, this.client);
+        if (event.options?.ONCE) {
+          this.client.once(event.name, execute);
+        } else {
+          this.client.on(event.name, execute);
+        }
+
+        this.client.events.set(event.name, event);
+      }
     }
   }
 
   public async load_commands(): Promise<void> {
-    const commandPath = path.join(__dirname, '..', 'commands');
-    const commandFiles = fs
-      .readdirSync(commandPath)
-      .filter((file) => file.endsWith('.js'));
-    for (const file of commandFiles) {
-      const filePath = path.join(commandPath, file);
-      this.clearCache(filePath);
-      const commandModule = await import(filePath);
-      const command: Command = commandModule.default;
-      if (!command.data || !command.data.name) {
-        console.log(`${file} does not have valid command data.`);
-        continue;
+    let commandArray: any[] = [];
+    const commandDir = this.load_files(`${process.cwd()}/dist/commands`);
+    for (const file of commandDir) {
+      if (file.endsWith('.ts') || file.endsWith('.js')) {
+        const commandPath = path.resolve(file);
+        this.clear_cache(commandPath);
+        const command: Command = (await import(commandPath)).default;
+        commandArray.push(command.data.toJSON());
+        this.client.commands.set(command.data.name, command);
       }
-
-      this.client.commands.set(command.data.name, command);
     }
+
+    this.client.on('ready', async () => {
+      this.client.application?.commands.set(commandArray);
+    });
   }
 
-  public async load_events(): Promise<void> {
-    const eventPath = path.join(__dirname, '..', 'events');
-    const eventFiles = fs
-      .readdirSync(eventPath)
-      .filter((file) => file.endsWith('.js'));
-    for (const file of eventFiles) {
-      const filePath = path.join(eventPath, file);
-      this.clearCache(filePath);
-      const eventModule = await import(filePath);
-      const event: Event = eventModule.default;
-      if (!event.name) {
-        console.log(`${file} does not have a name.`);
-        continue;
-      }
-
-      const execute = (...args: any[]) => event.execute(...args, this.client);
-      if (event.options?.ONCE) {
-        this.client.once(event.name, execute);
+  private load_files(dirName: string): string[] {
+    let results: string[] = [];
+    const list = fs.readdirSync(dirName);
+    list.forEach((file) => {
+      file = path.resolve(dirName, file);
+      const stat = fs.statSync(file);
+      if (stat && stat.isDirectory()) {
+        results = results.concat(this.load_files(file));
       } else {
-        this.client.on(event.name, execute);
+        results.push(file);
       }
+    });
+
+    return results;
+  }
+
+  private clear_cache(modulePath: string): void {
+    const resolvedPath = require.resolve(modulePath);
+    if (require.cache[resolvedPath]) {
+      delete require.cache[resolvedPath];
     }
   }
 }
